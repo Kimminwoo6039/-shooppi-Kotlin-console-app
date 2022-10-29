b749a08ca32ff75c6fa3e04213f59ddffc8fa7e4
cas90 <qkqhemforhs1@gamail.com>
2022-10-29 10:39:41 +0900

    Main.kt 실행 2022/10/29

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,8 @@ import screen.ShoppinHome
 
 fun main(args: Array<String>) {
 
-    println("hello wolrd!")
-
-/*
    val ShoppinHome = ShoppinHome()
-   ShoppinHome.start()*/
+   ShoppinHome.start()
 
 
-
-    fun max(a:Int,b:Int):Int{
-        return if (a>b) a else b
-    }
-
-    println(max(1,2))
 }
